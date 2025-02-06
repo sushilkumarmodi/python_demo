51ab2c4cb41a36af4cc45a0810670615e0ebd080
sushilkumarmodi <sushilct25@gmail.com>
2025-02-06 22:14:08 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 def hello():     
        return "welcome to the flask tutorials"
 if __name__ == "__main__":
-     app.run(host ='0.0.0.0', debug = false) 
+     app.run(host ='0.0.0.0', debug = False) 
